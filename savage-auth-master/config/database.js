6de0a12344353c8a1a2a8653f94226feec207f95
William Sanchez <williamsanchezdev@gmail.com>
2022-05-30 23:42:22 -0400

    Add files via upload

---
+++ savage-auth-master/config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url': 'mongodb+srv://Will:password1234@cluster2.vhhzs.mongodb.net/Cluster2?retryWrites=true&w=majority',
+    'dbName': 'messages'
+};
